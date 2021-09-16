fcc2ea599b8af382678eea84eb4550ef713d97f0
PreritSM <56505861+PreritSM@users.noreply.github.com>
2021-09-16 22:16:31 +0530

    Update main.py
    added login, logout and change password functionalities

--- main.py
+++ main.py
@@ -1,99 +1,99 @@
-from pyad import *
-import flask
-from pymongo import MongoClient
-import certifi
-import json
-import socket
-from flask import Flask, request
-from datetime import datetime,time,timedelta
-import sublist3r
-import threading
-import time
-import requests
+import glob
+glob.setglob()
+from custom_func import *
+from flask import Flask, jsonify, request, render_template, redirect, url_for, make_response
+from dotenv import load_dotenv
+from datetime import datetime
+
+load_dotenv()
 
 app = Flask(_name_)
+app.config['SECRET_KEY']=os.getenv("PASS_KEY")
 
-client=MongoClient('mongodb+srv://cirius:MVA1IzOr8GCYoSv8@cluster0.53e13.mongodb.net/?retryWrites=true&w=majority',tlsCAFile=certifi.where())
-
+client=MongoClient(glob.conn_str,tlsCAFile=certifi.where())
 #Domain Name in explore functions
 
-def exploreusers(ide):
-    q = adquery.ADQuery()
+def token_required(f):
+   @wraps(f)
+   def decorator(*args, **kwargs):
+       token=None
+       if 'x-access-tokens' in request.cookies:
+           token=request.cookies['x-access-tokens']
+       if not token:
+            return render_template('login.html',error="Token not provided, login to get your token.")
+            #return jsonify({"Message":"Token not provided."})
+       try:
+            data=jwt.decode(token,app.config['SECRET_KEY'])
+       except:
+           return render_template('login.html',error="Token is invalid, login to get your token.")
+
+       return f(*args, **kwargs)
+   return decorator
+
+@app.route('/login',methods=["POST", "GET"])
+def login():
+    if request.method=="GET":
+        return render_template('login.html')
+    password=request.form['password']
     db=client.db
-    conn=db.users
-    q.execute_query(
-        attributes = ["name","userprincipalname","memberof"],
-        where_clause = "objectClass = '*'",
-        base_dn = "CN=Users, DC=lab01, DC=local"
-    )
-    if(ide!=1):
-        for row in q.get_results():
-            if(row["userprincipalname"] is not None):
-                a=[]
-                if(row["memberof"] is not None):
-                    for i in row["memberof"]:
-                        a.append(i)
-                a=json.dumps(a)
-                row["memberof"]=a
-                if(conn.find_one(row["userprincipalname"]) is None):
-                    conn.insert_one(row)
-    else:
-        up=[]
-        for row in q.get_results():
-            if(row["userprincipalname"] is not None):
-                a=[]
-                if(row["memberof"] is not None):
-                    for i in row["memberof"]:
-                        a.append(i)
-                a=json.dumps(a)
-                row["memberof"]=a
-                up.append(row)
-        conn.insert_many(up)        
-        
-def exploredevices(ide):
-    q = adquery.ADQuery()
+    to_check=db.first
+    for all_p in to_check.find():
+        stored_password=all_p['password']
+    chck=verify_password(stored_password,password)
+    if chck==False:
+        return render_template('login.html',error="Incorrect password provided.")
+    all_chars=string.ascii_letters+string.digits
+    random_string=''.join(random.choices(all_chars, k=20))
+    token = jwt.encode({'random': random_string, 'exp' : datetime.utcnow() + timedelta(minutes=300)}, app.config['SECRET_KEY'])
+    resp=make_response(render_template('credentials.html'))
+    resp.set_cookie('x-access-tokens',token)
+    return resp
+
+#Change Password
+@app.route('/change_password',methods=["POST"])
+@token_required
+def change_password():
+    old_password=request.form['old_password']
+    new_password=request.form['new_password']
+    cnf_new_password=request.form['cnf_new_password']
+    if new_password != cnf_new_password:
+        return render_template('.html',error="Passwords do not match.")
     db=client.db
-    conn=db.computers
-    q.execute_query(
-        attributes = ["dnshostname","cn","operatingsystem","operatingsystemhotfix","operatingsystemservicepack","operatingsystemversion","memberof"],
-        where_clause = "objectClass = '*'",
-        base_dn = "CN=Computers, DC=lab01, DC=local"
-    )
-    if(ide!=1):
-        data=q.get_results()
-        for row in data:
-            if(row["dnshostname"] is None):
-                continue
-            print(type(row))
-            a=[]
-            if(row["memberof"] is not None):
-                for i in row["memberof"]:
-                    a.append(i)
-            a=json.dumps(a)
-            row["memberof"]=a
-            ip=""
-            if(row["dnshostname"] is not None):
-                ip=socket.gethostbyname(row["dnshostname"])
-            row["ip"]=ip
-            if(conn.find(row["dnshostname"]) is None):
-                conn.insert_one(row)
-    else:  
-        up=[]
-        data=q.get_results()
-        for row in data:
-            if(row["dnshostname"] is None):
-                continue
-            print(type(row))
-            a=[]
-            if(row["memberof"] is not None):
-                for i in row["memberof"]:
-                    a.append(i)
-            a=json.dumps(a)
-            row["memberof"]=a
-            ip=""
-            if(row["dnshostname"] is not None):
-                ip=socket.gethostbyname(row["dnshostname"])
-            row["ip"]=ip
-            up.append(row)
-            print(row["ip"],row["operatingsystem"])
-        conn.insert_many(up)
+    to_check=db.first
+    for all_p in to_check.find():
+        stored_password=all_p['password']
+    chck=verify_password(stored_password,old_password)
+    if chck==False:
+        return render_template('changepassword.html',error="Incorrect old password.")
+    hash_password(new_password)
+    return redirect(url_for('logout'))
+
+@app.route('/logout',methods=['GET'])
+@token_required
+def logout():
+    resp = make_response(redirect(url_for('login')))
+    resp.delete_cookie('x-access-tokens')
+    return resp
+
+def apprun():
+    cwd=os.getcwd()
+    app.run(debug=True, host='0.0.0.0', port=5000 , ssl_context=(cwd+'\\cert.pem', cwd+'\\key.pem'), use_reloader=False)
+
+if _name_ == '_main_':
+    cwd=os.getcwd()
+    #test()
+    pr=subprocess.Popen("powershell Get-NetFirewallRule -DisplayName \"AssetMonitoring\"",stdout=subprocess.PIPE)
+    pr.communicate()[0]
+    
+    if(pr.returncode==1):
+        input("The tool requires port 5000 to be open inside the domain network.A Prompt would ask for adminsitrator privileges to create a rule in firewall for the port.\nPress Enter to continue...")
+        print("Creating the rule in firewall")
+        subprocess.Popen("powershell -noprofile -command \"&{Start-Process powershell -Verb Runas -ArgumentList '-noprofile -executionpolicy bypass -file "+cwd+"\\create_rule.ps1'}\"",shell=True)    
+    
+    t=threading.Thread(target=apprun, daemon=True).start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Exiting")
+        exit(0)
